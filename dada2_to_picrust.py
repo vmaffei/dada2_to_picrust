0aa57ea6b870727ddc559655a3b8470838a99c27
Gene Blanchard <gblanchard4@gmail.com>
2017-02-15 12:46:10 -0600

    get pynasty

--- dada2_to_picrust.py
+++ dada2_to_picrust.py
@@ -1,16 +1,47 @@
 #!/usr/bin/env python
 import argparse
+import qiime_default_reference as qdr
 import pandas
 from Bio import SeqIO
-
-import rpy2.robjects as robjects
-from rpy2.robjects.packages import importr
+from skbio.parse.sequences import parse_fasta
+from skbio.alignment import SequenceCollection, Alignment
+from skbio.sequence import DNASequence
+from pynast.util import pynast_seqs, pairwise_alignment_methods
+from pynast.logger import NastLogger
 
 __authors__ = "Gene Blanchard, Vince Maffei"
 
 '''
 dada2_to_picrust conversion using @vmaffei's workflow
 '''
+
+def remove_quotes(line):
+    line = line.replace('"', '')
+    return line
+
+
+def dict_to_repset(repsetfile, otudict):
+    with open(repsetfile, 'w') as outhandle:
+        for key in otudict:
+            outhandle.write(">{}\n{}\n".format(key, otudict[key]))
+
+
+def format_seqtab_to_repset_table(seqtab):
+    otutable = seqtab + ".otutable"
+    rep_set = seqtab + ".repset"
+    parsed_lines = []
+    with open(seqtab) as seqtab_handle, open(otutable, 'w') as otuhandle:
+        for index, line in enumerate(seqtab_handle):
+            if index == 0:
+                otus = map(remove_quotes, line.rstrip('\n').split(','))[1::]
+                otu_dict = {"node{}".format(n): seq for n, seq in enumerate(otus)}
+                dict_to_repset(rep_set, otu_dict)
+                parsed_lines.append(["#OTU ID"]+["node"+n for n in (map(str, range(0, len(otus))))])
+            else:
+                parsed_lines.append(remove_quotes(line).rstrip().split(','))
+
+        otuhandle.write('\n'.join(['\t'.join(line) for line in list(np.array(parsed_lines).T)]))
+
 
 def subset_img(seqtab, ko, ref, img):
     img_ko = pd.DataFrame.from_csv(ko, sep='\t')
@@ -25,8 +56,33 @@ def subset_img(seqtab, ko, ref, img):
                 SeqIO.write(ref_dict[key], handle, "fasta")
             except KeyError:
                 pass
-    
-    
+
+def pynasty(fasta, template_file, min_pct=0.75, min_len=90):
+    # Prepare template
+    with open(template_file, 'r') as template_h:
+        template = [(seqid, seq.replace('.', '-').upper()) for seqid, seq in parse_fasta(template_h)]
+    template = Alignment.from_fasta_records(template, DNASequence, validate=True)
+    # Prepare logger
+    logger = NastLogger("pynast.log")
+    # Align
+    # Parse fasta
+    with open(fasta, 'r') as fasta_h:
+        inseqs = parse_fasta(fasta_h)
+        pynast_aligned, pynast_failed = pynast_seqs(inseqs, template, min_pct=min_pct, min_len=min_len, align_unaligned_seqs_f='blast', logger=logger)
+    for i, seq in enumerate(pynast_failed):
+        seq_record = DNASequence(str(seq), id=seq.Name)
+        pynast_failed[i] = seq_record
+    pynast_failed = SequenceCollection(pynast_failed)
+
+    for i, seq in enumerate(pynast_aligned):
+        seq_record = DNASequence(str(seq), id=seq.Name)
+        pynast_aligned[i] = seq_record
+    pynast_aligned = Alignment(pynast_aligned)
+
+    with open("pynast_failed.fa", 'w') as failed_h:
+        failed_h.write(pynast_failed.to_fasta())
+    with open("pynast_aligned.fa", 'w') as pynast_h:
+        pynast_h.write(pynast_aligned.to_fasta())
 
 def main():
     # Argument Parser
@@ -40,6 +96,8 @@ def main():
     parser.add_argument('-r', '--ref', dest='ref', required=True, help='File: gg_13_5.fasta, see http://greengenes.secondgenome.com/downloads/database/13_5')
     # GG img 
     parser.add_argument('-i', '--img', dest='img', required=True, help='File: gg_13_5_img.txt, see http://greengenes.secondgenome.com/downloads/database/13_5')
+    # Template
+    parser.add_argument('-t', '--template', dest='template', default=qdr.get_template_alignmet(), help='Pynast Alignmet Template - Default:85_otus.pynast.fasta')
     
     # Output file
     parser.add_argument('-o', '--output', dest='output', help='The output file')
@@ -54,7 +112,9 @@ def main():
     ref = "data/gg_13_5.fasta"
     img = "data/gg_13_5_img.txt"
 
-    # Do stuff here
+    # Qiime files from seqtab
+    format_seqtab_to_repset_table(seqtab)
+    
 
 if __name__ == '__main__':
     main()
